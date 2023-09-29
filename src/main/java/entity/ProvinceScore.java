b5995c8dfd5e803f7b75382531d5c8443e8eedf1
Oscar Kolodziejczyk <oscar.kolodziejczyk@mail.utoronto.ca>
2023-09-29 01:32:23 -0400

    Outline structure of the project including some temporary classes to begin with

---
+++ src/main/java/entity/ProvinceScore.java
@@ -0,0 +1,12 @@
+package entity;
+
+import java.util.ArrayList;
+
+public class ProvinceScore {
+
+    private int score;
+
+    private String province;
+
+    private ArrayList<String> parameters;
+}
