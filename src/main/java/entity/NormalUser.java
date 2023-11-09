8e801dbd4058d55f49d5f6dc1a127525f01c5c63
mohammedhussain <moham21387@gmail.com>
2023-11-09 15:48:36 -0500

    adding constructor to NormalUser

--- src/main/java/entity/NormalUser.java
+++ src/main/java/entity/NormalUser.java
@@ -4,10 +4,18 @@ import java.util.List;
 
 public class NormalUser implements User {
     private int userID;
-    private String name;
     private String username;
     private String password;
     private WeatherPref weatherPreference;
     private List<City> cityList;
+
+    public NormalUser(int userID, String username, String password, WeatherPref weatherPreference, List<City> cityList)
+    {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.weatherPreference = weatherPreference;
+        this.cityList = cityList;
+    }
 
 }
