d4fbef85602adf16a95aad6d4a94a31bc96b3593
mohammedhussain <moham21387@gmail.com>
2023-11-09 17:09:06 -0500

    creates a txt file which stores every NormalUser's userid, name, and list of cities (still needs to be tested as well also creating constructor for city class

--- src/main/java/entity/NormalUser.java
+++ src/main/java/entity/NormalUser.java
@@ -1,6 +1,9 @@
 package entity;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class NormalUser implements User {
     private int userID;
@@ -9,6 +12,8 @@ public class NormalUser implements User {
     private WeatherPref weatherPreference;
     private List<City> cityList;
 
+    private static List<NormalUser> savedCities = new ArrayList<>();
+
     public NormalUser(int userID, String username, String password, WeatherPref weatherPreference, List<City> cityList)
     {
         this.userID = userID;
@@ -16,6 +21,28 @@ public class NormalUser implements User {
         this.password = password;
         this.weatherPreference = weatherPreference;
         this.cityList = cityList;
+
+        savedCities.add(this);
+    }
+
+    public static void main (String[] args)
+    { //(hopefully) this creates a txt file which stores every NormalUser's userid, name, and list of cities
+        String txtfile = "savedUsers.txt";
+        try {
+            FileWriter fileWriter = new FileWriter(txtfile);
+            for (NormalUser user: savedCities)
+            {
+                fileWriter.write(Integer.toString(user.userID) + " " + user.username);
+                for (City city: user.cityList)
+                {
+                    fileWriter.write(city.name);
+                }
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
