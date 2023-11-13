3e72dcb46666e2a2fbef2da42fd1faad454e42f3
mohammedhussain <moham21387@gmail.com>
2023-11-13 11:58:13 -0500

    creating constructors for weatherData, weatherPref, weatherScore. added instance attribute for weatherscore

--- src/main/java/entity/WeatherData.java
+++ src/main/java/entity/WeatherData.java
@@ -1,8 +1,15 @@
 package entity;
 
 public class WeatherData {
-    private double percipitation;
+    private double precipitation;
     private double humidity;
     private double temperature;
+
+    public WeatherData (double precipitation, double humidity, double temperature)
+    {
+        this.precipitation = precipitation;
+        this.humidity = humidity;
+        this.temperature = temperature;
+    }
 
 }
--- src/main/java/entity/WeatherPref.java
+++ src/main/java/entity/WeatherPref.java
@@ -7,4 +7,14 @@ public class WeatherPref {
     private int userTempPreferenceScore;
     private int userHumidityPreferenceScore;
     private int userPrecipitationPreferenceScore;
+
+    public WeatherPref(int userTempPreference, int userHumidityPreference, int userPrecipitationPreference, int userTempPreferenceScore, int userHumidityPreferenceScore, int userPrecipitationPreferenceScore)
+    {
+        this.userTempPreference = userTempPreference;
+        this.userHumidityPreference = userHumidityPreference;
+        this.userPrecipitationPreference = userPrecipitationPreference;
+        this.userTempPreferenceScore = userTempPreferenceScore;
+        this.userHumidityPreferenceScore = userHumidityPreferenceScore;
+        this.userPrecipitationPreferenceScore = userPrecipitationPreferenceScore;
+    }
 }
