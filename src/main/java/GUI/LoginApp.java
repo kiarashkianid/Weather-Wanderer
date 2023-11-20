7f4624814f690aa536b2d89ef86ec1be82d3a453
Polymatth <matthewamacq@gmail.com>
2023-11-20 11:24:07 -0500

    Edited UI in LoginApp. Implemented LoginPresenter which returns a string to LoginApp based on LoginInteractor. Need to implement LoginInteractor

--- src/main/java/GUI/LoginApp.java
+++ src/main/java/GUI/LoginApp.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import interface_adapter.login.LoginPresenter;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -12,14 +14,11 @@ public class LoginApp {
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JButton loginButton;
-    private JButton logoutButton;
     private JLabel statusLabel;
     private JLabel backgroundLabel; // Add a JLabel for the background
-    private String validUsername = "user";
-    private String validPassword = "pass";
 
     public LoginApp() {
-        frame = new JFrame("Login/Logout App");
+        frame = new JFrame("Login App");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(650, 400);
 
@@ -27,71 +26,77 @@ public class LoginApp {
         frame.add(layeredPane);
 
         panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 2));
+        panel.setLayout(new GridBagLayout());
 
         usernameField = new JTextField();
         passwordField = new JPasswordField();
         loginButton = new JButton("Login");
-        logoutButton = new JButton("Logout");
         statusLabel = new JLabel("");
         backgroundLabel = new JLabel();
 
-        panel.add(new JLabel("Username:"));
-        panel.add(usernameField);
-        panel.add(new JLabel("Password:"));
-        panel.add(passwordField);
-        panel.add(loginButton);
-        panel.add(logoutButton);
+        usernameField.setPreferredSize(new Dimension(150, 150));
+        passwordField.setPreferredSize(new Dimension(150, 150));
+
+        GridBagConstraints constrainUserLabel = new GridBagConstraints();
+        constrainUserLabel.gridx = 0;
+        constrainUserLabel.gridy = 0;
+        GridBagConstraints constrainUserField = new GridBagConstraints();
+        constrainUserField.gridx = 1;
+        constrainUserField.gridy = 0;
+        constrainUserField.gridwidth = 2;
+        constrainUserField.anchor = GridBagConstraints.WEST;
+        constrainUserField.fill = GridBagConstraints.HORIZONTAL;
+        GridBagConstraints constrainPassLabel = new GridBagConstraints();
+        constrainPassLabel.gridx = 0;
+        constrainPassLabel.gridy = 1;
+        GridBagConstraints constrainPassField = new GridBagConstraints();
+        constrainPassField.gridx = 1;
+        constrainPassField.gridy = 1;
+        constrainPassField.gridwidth = 2;
+        constrainPassField.anchor = GridBagConstraints.WEST;
+        constrainPassField.fill = GridBagConstraints.HORIZONTAL;
+        GridBagConstraints constrainLogin = new GridBagConstraints();
+        constrainLogin.gridx = 1;
+        constrainLogin.gridy = 2;
+
+        panel.add(new JLabel("Username:"), constrainUserLabel);
+        panel.add(usernameField, constrainUserField);
+        panel.add(new JLabel("Password:"), constrainPassLabel);
+        panel.add(passwordField, constrainPassField);
+        panel.add(loginButton, constrainLogin);
 
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String username = usernameField.getText();
                 String password = new String(passwordField.getPassword());
-
-                if (authenticate(username, password)) {
-                    statusLabel.setText("Login successful.");
-                    loginButton.setEnabled(false);
-                    logoutButton.setEnabled(true);
+                String message = authenticate(username, password);
+                if (message.toLowerCase().contains("success")) {
                     usernameField.setEnabled(false);
                     passwordField.setEnabled(false);
-                } else {
-                    statusLabel.setText("Login failed. Invalid username or password.");
+                    loginButton.setEnabled(false);
                 }
+                statusLabel.setText(message);
+
             }
         });
-
-        logoutButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                statusLabel.setText("");
-                loginButton.setEnabled(true);
-                logoutButton.setEnabled(false);
-                usernameField.setEnabled(true);
-                passwordField.setEnabled(true);
-                usernameField.setText("");
-                passwordField.setText("");
-            }
-        });
-
-        logoutButton.setEnabled(false);
 
         // Load and set your GIF file as the background
-        ImageIcon backgroundIcon = new ImageIcon("C:\\Users\\Kiarash\\IdeaProjects\\Weather-Wanderer\\src\\main\\java\\GUI\\loginPageBackground.gif");
+        ImageIcon backgroundIcon = new ImageIcon("src/main/java/GUI/loginPageBackground.gif");
         backgroundLabel.setIcon(backgroundIcon);
         backgroundLabel.setBounds(0, 0, frame.getWidth(), frame.getHeight());
         layeredPane.add(backgroundLabel, JLayeredPane.DEFAULT_LAYER);
 
         panel.setBounds(100, 100, 400, 200); // Set bounds for the panel
-        statusLabel.setBounds(100, 350, 400, 30); // Set bounds for the statusLabel
+        statusLabel.setBounds(100, 60, 400, 30); // Set bounds for the statusLabel
         layeredPane.add(panel, JLayeredPane.PALETTE_LAYER);
-        layeredPane.add(statusLabel, JLayeredPane.PALETTE_LAYER);
+        layeredPane.add(statusLabel, JLayeredPane.POPUP_LAYER);
 
         frame.setVisible(true);
     }
 
-    private boolean authenticate(String username, String password) {
-        return username.equals(validUsername) && password.equals(validPassword);
+    private String authenticate(String username, String password) {
+        return LoginPresenter.initializeLogin(username, password);
     }
 
     public static void main(String[] args) {
--- src/main/java/use_case/normaluser/Login/LoginInteractor.java
+++ src/main/java/use_case/normaluser/Login/LoginInteractor.java
@@ -1,4 +1,14 @@
 package use_case.normaluser.Login;
 
 public class LoginInteractor {
+    public static boolean authenticate(String username, String password) {
+        if (username.equals("user") && password.equals("pass")){
+        return true;}
+        else {return false;}
+        //TODO: Code Interactor to pull from DB via Gateway
+    }
+
+    public static void userIdToMemory(int id){
+        //TODO: Add to Memory
+    }
 }
