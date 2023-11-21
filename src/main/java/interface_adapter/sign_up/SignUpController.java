6494e3121bb89069c1dcae75d5cddcac0bd9d6ae
mohammedhussain <moham21387@gmail.com>
2023-11-21 15:59:45 -0500

    creating signup controller

--- src/main/java/interface_adapter/sign_up/SignUpController.java
+++ src/main/java/interface_adapter/sign_up/SignUpController.java
@@ -1,4 +1,19 @@
 package interface_adapter.sign_up;
 
+import use_case.normaluser.SignUp.SignUpInputBoundary;
+import use_case.normaluser.SignUp.SignUpInputData;
+
 public class SignUpController {
+    final SignUpInputBoundary userSignupUseCaseInteractor;
+    public SignUpController(SignUpInputBoundary userSignupUseCaseInteractor) {
+        this.userSignupUseCaseInteractor = userSignupUseCaseInteractor;
+    }
+
+    public void execute(String username, String password1, String password2) {
+        SignUpInputData signupInputData = new SignUpInputData(
+                username, password1, password2);
+
+        userSignupUseCaseInteractor.execute(signupInputData);
+    }
+
 }
