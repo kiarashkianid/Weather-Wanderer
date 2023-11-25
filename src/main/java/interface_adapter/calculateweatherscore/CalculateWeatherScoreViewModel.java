658fd7c6f116a3ba18707650864996f1203731cc
Oscar Kolodziejczyk <oscar.kolodziejczyk@mail.utoronto.ca>
2023-11-25 00:30:12 -0500

    Implementation of ChoosePreferences almost completed; working, just needs bug fixes.

--- src/main/java/interface_adapter/calculateweatherscore/CalculateWeatherScoreViewModel.java
+++ src/main/java/interface_adapter/calculateweatherscore/CalculateWeatherScoreViewModel.java
@@ -1,4 +1,27 @@
 package interface_adapter.calculateweatherscore;
 
-public class CalculateWeatherScoreViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+
+public class CalculateWeatherScoreViewModel extends ViewModel {
+    public CalculateWeatherScoreViewModel(String viewName) {
+        super(viewName);
+    }
+
+    public void setState(CalculateWeatherScoreState calculateWeatherScoreState) {
+    }
+    public CalculateWeatherScoreState getState(){
+        return new CalculateWeatherScoreState();
+    }
+
+    @Override
+    public void firePropertyChanged() {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+    }
 }
