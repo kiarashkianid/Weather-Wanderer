1999b10c59b600ce2e8a3359d275a62fa117b68d
Kiarash Kianidehkordi <81038531+kiarashkianid@users.noreply.github.com>
2023-11-27 12:02:24 -0500

    2023-11-27

--- src/main/java/View/ChoosePreferencesView.java
+++ src/main/java/View/ChoosePreferencesView.java
@@ -8,8 +8,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
@@ -158,7 +156,6 @@ public class ChoosePreferencesView extends JPanel implements ActionListener, Pro
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
     }
 
     @Override
--- src/main/java/interface_adapter/calculate_score/CalculateScoreState.java
+++ src/main/java/interface_adapter/calculate_score/CalculateScoreState.java
@@ -1,10 +1,27 @@
 package interface_adapter.calculate_score;
 //TODO
 
+import entity.City;
+import entity.WeatherPref;
 import use_case.choosepreferences.ChooseOutputData;
 
+import java.util.ArrayList;
+
 public class CalculateScoreState {
+    private WeatherPref weatherPref;
+    private ArrayList<City> addedCities;
+
+    public CalculateScoreState(){
+    }
+
+    public CalculateScoreState(CalculateScoreState copy) {
+        weatherPref=copy.weatherPref;
+        addedCities=copy.addedCities;
+    }
+
     public void setPreferences(ChooseOutputData preferences) {
+        this.weatherPref=preferences.getWeatherPref();
+        this.addedCities=preferences.getCityList();;
 
     }
 
