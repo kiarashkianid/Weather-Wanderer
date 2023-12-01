3fff4600d5934e3c4f1cc48213be3bff04e40a40
Polymatth <75713942+Polymatth@users.noreply.github.com>
2023-11-30 23:24:47 -0500

    Changed view name to match LoginView

--- src/main/java/interface_adapter/login/LoginViewModel.java
+++ src/main/java/interface_adapter/login/LoginViewModel.java
@@ -1,18 +1,20 @@
 package interface_adapter.login;
+import interface_adapter.ViewModel;
+
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-import interface_adapter.ViewModel;
+
 public class LoginViewModel extends ViewModel{
     public static final String TITLE_LABEL = "Log In View";
     public static final String USERNAME_LABEL = "Enter username";
     public static final String PASSWORD_LABEL = "Enter password";
 
-    public static final String LOGIN_BUTTON_LABEL = "Log in";
+    public static final String LOGIN_BUTTON_LABEL = "Login";
 
     private LoginState state = new LoginState();
 
     public LoginViewModel() {
-        super("log in");
+        super("login");
     }
 
     public void setState(LoginState state) {
@@ -21,8 +23,6 @@ public class LoginViewModel extends ViewModel{
 
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
-    // This is what the Signup Presenter will call to let the ViewModel know
-    // to alert the View
     public void firePropertyChanged() {
         support.firePropertyChange("state", null, this.state);
     }
